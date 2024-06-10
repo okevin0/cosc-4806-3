fa11e6091cc23e554c43447634918eb3500998e4
Kevin Lin <kevin.shuminglin@gmail.com>
2024-06-10 03:15:52 +0000

    add secret and update database config

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
+define('DB_HOST',         'n3s.h.filess.io');
+define('DB_USER',         'cosc4806_smallpick'); 
 define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
+define('DB_DATABASE',     'cosc4806_smallpick');
 define('DB_PORT',         '3305');
